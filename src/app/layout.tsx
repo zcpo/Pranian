5d73a93a5c3ec2eca914b96c0796e0f94e82e8f1
Prop Theme <proptheme@gmail.com>
2025-11-27 12:34:38 +0000

    debuge fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link rel='stylesheet' href='https://unpkg.com/plyr@3/dist/plyr.css' />
         <script src="https://cdn.rawgit.com/video-dev/hls.js/18bb552/dist/hls.min.js" async></script>
         <script src='https://unpkg.com/plyr@3' async></script>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
