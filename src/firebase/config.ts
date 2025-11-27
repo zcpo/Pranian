b3d6615048c580fd706737aca215a84c458e3b36
Prop Theme <proptheme@gmail.com>
2025-11-27 17:04:40 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-481934975-de4a8",
   "appId": "1:704301395500:web:9d47426ee6b3cb95f3ab89",
+  "storageBucket": "studio-481934975-de4a8.appspot.com",
   "apiKey": "AIzaSyACMSObxsmU219RJgNz7rQYDohM8JqvL4c",
   "authDomain": "studio-481934975-de4a8.firebaseapp.com",
   "measurementId": "",
