11a87cd9072707bbee72910be865ce302cfd0440
Prop Theme <proptheme@gmail.com>
2025-11-27 14:59:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-481934975-de4a8",
+  "appId": "1:704301395500:web:9d47426ee6b3cb95f3ab89",
+  "apiKey": "AIzaSyACMSObxsmU219RJgNz7rQYDohM8JqvL4c",
+  "authDomain": "studio-481934975-de4a8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "704301395500"
+};
