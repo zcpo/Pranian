d4f161575186c6c99f90d15d686bfc11a928b230
Prop Theme <proptheme@gmail.com>
2025-12-06 08:19:19 +0000

    error: failed to push some refs to 'https://github.com/zcpo/Pranian.git'

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,31 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '**',
       },
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      }
     ],
   },
 };
